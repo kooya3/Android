5dd3588feb93b092934b81af1056364efb2acc76
Elyees Tatua <elyeestatua@gmail.com>
2024-07-04 23:25:30 +0300

    build(init): Connecting firebase

--- firebase.ts
+++ firebase.ts
@@ -3,14 +3,15 @@ import { getMessaging, getToken, isSupported } from "firebase/messaging";
 
 // Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: null,
-  authDomain: null,
-  projectId: null,
-  storageBucket: null,
-  messagingSenderId: null,
-  appId: null,
-  measurementId: null,
+  apiKey: "AIzaSyA73msQuS_d0uHLkji9-WQqiSMRO5_YIes",
+  authDomain: "android-demo-973bf.firebaseapp.com",
+  projectId: "android-demo-973bf",
+  storageBucket: "android-demo-973bf.appspot.com",
+  messagingSenderId: "841093999622",
+  appId: "1:841093999622:web:9a82e7190c56dab0b52787",
+  measurementId: "G-160FWC8FFJ"
 };
+
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
